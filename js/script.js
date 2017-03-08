9863aad633fae6d5a79718de8ff7a7b149cee541
gjoche01 <greta.jochem@tufts.edu>
2017-03-07 22:34:54 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+/* SECTION ONE START through our last mother*/
+
+/*SECTION TWO START throgh Fault line swell*/
+
+/* SECTION THREE START through curly hair routinue */
+
+
+/*SECTION FOUR START through mother*/
+
+/*SECTION FIVE START through parting shot*/
